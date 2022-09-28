1c3fcdbfd263120767abcf541769b242a9e70418
Dev-Fouad <114316696+Dev-Fouad@users.noreply.github.com>
2022-09-28 13:33:04 +0100

    Add files via upload

---
+++ lesson1.js
@@ -0,0 +1,43 @@
+document.getElementById('demo').style.fontSize = "35px";
+
+document.getElementById('text')
+
+// document.getElementById('hero').innerHTML= 'hello world';
+//  document.getElementById('hero').innerHTML= 'hello world';
+//  console.log('happy')
+
+//   let is use to declare a varable
+// let x = '16'
+// let y ='fouad';
+// let z = x+y
+
+
+
+// array_boys = [2, 'dayo', {}, false]    Array is use to store data types..
+// array_boys =[
+//     person1 = {
+//         firstName:"Ajala",
+//         gender:"female",
+//         age:12
+//     } ,
+//     person2 = {
+//         firstName:"temi",
+//         gender:"female",
+//         age:15
+//     },
+//     person3 = {
+//         firstName:"tolu",
+//         gender:"male",
+//         age:26
+//     },
+//     'dayo',
+//     false,
+//     34,
+
+// ]
+// console.log(array_boys.length);
+// console.log(typeof(array_boys));
+// console.log(array_boys[4]);
+
+
+
